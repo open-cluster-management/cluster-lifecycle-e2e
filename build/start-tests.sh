3e1fb984ef8ae298bd48b9a4187af7133a52fb1f
Leena Jawale <ljawale@redhat.com>
2020-12-17 16:15:37 -0500

    update results files (#7)

--- build/start-tests.sh
+++ build/start-tests.sh
@@ -5,9 +5,9 @@
 echo "Initiating tests..."
 
 if [[ $TEST_GROUP == "import" ]]; then
-    ginkgo -v -focus="import" -skip="" -trace -debug e2e-test.test -- -v=3
+    ginkgo -v -focus="import" -skip="" --reportFile=$REPORT_FILE_IMPORT -trace -debug e2e-test.test -- -v=3
 elif [[ $TEST_GROUP == "provision-all" ]]; then
     ginkgo -v -focus="create" -skip="" -p -trace --debug e2e-test.test -- -v=3
 elif [[ $TEST_GROUP == "destroy" ]]; then
-    ginkgo -v -focus="detach" -skip="" -trace -debug e2e-test.test -- -v=3
+    ginkgo -v -focus="detach" -skip="" --reportFile=$REPORT_FILE_DETACH -trace -debug e2e-test.test -- -v=3
 fi
