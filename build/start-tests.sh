38eff89da45d751d7fd7d8565ad3a6c79f166a48
Leena Jawale <ljawale@redhat.com>
2021-01-11 15:02:10 -0500

    add baremetal test in start script
    Signed-off-by: Leena Jawale <ljawale@redhat.com>

--- build/start-tests.sh
+++ build/start-tests.sh
@@ -11,5 +11,7 @@ elif [[ $TEST_GROUP == "provision-all" ]]; then
 elif [[ $TEST_GROUP == "destroy" ]]; then
     ginkgo -v -focus="detach|destroy" -p -trace -debug e2e-test.test -- -v=3 -owner="ginkgo-$TRAVIS_BUILD_ID" -report-file=$REPORT_FILE_DESTROY
 elif [[ $TEST_GROUP == "metrics" ]]; then
-    ginkgo -v -focus="metrics" --reportFile=$REPORT_FILE_METRICS -trace -debug e2e-test.test -- -v=3
+    ginkgo -v -focus="metrics" -skip="" --reportFile=$REPORT_FILE_METRICS -trace -debug e2e-test.test -- -v=3
+elif [[ $TEST_GROUP == "baremetal" ]]; then
+    ginkgo -v  -focus="baremetal" -skip="" -trace --debug e2e-test.test -- -v=3 
 fi
