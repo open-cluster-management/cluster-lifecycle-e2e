f3710b6e71218d4365d483e21ae19f9dc447bff9
Dominique Vernier <dvernier@redhat.com>
2021-01-13 08:40:06 -0500

    Use a different REPORT_FILE for METRICS

--- build/start-tests.sh
+++ build/start-tests.sh
@@ -11,5 +11,5 @@ elif [[ $TEST_GROUP == "provision-all" ]]; then
 elif [[ $TEST_GROUP == "destroy" ]]; then
     ginkgo -v -focus="detach" -skip="" --reportFile=$REPORT_FILE_DETACH -trace -debug e2e-test.test -- -v=3
 elif [[ $TEST_GROUP == "metrics" ]]; then
-    ginkgo -v -focus="metrics" -skip="" --reportFile=$REPORT_FILE_DETACH -trace -debug e2e-test.test -- -v=3
+    ginkgo -v -focus="metrics" -skip="" --reportFile=$REPORT_FILE_METRICS -trace -debug e2e-test.test -- -v=3
 fi
