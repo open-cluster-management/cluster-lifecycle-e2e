d077dbab07f3431ec353f4dbfaff0113cda8cc8d
Dominique Vernier <dvernier@redhat.com>
2021-05-19 17:18:18 -0400

    Increase number of nodes on destroy
    Signed-off-by: Dominique Vernier <dvernier@redhat.com>

--- build/start-tests.sh
+++ build/start-tests.sh
@@ -3,14 +3,14 @@
 # Copyright (c) 2020 Red Hat, Inc.
 
 echo "Initiating tests..."
-echo "Tests start at "$(date)
+echo "Tests start $TEST_GROUP at "$(date)
 
 if [[ $TEST_GROUP == "import" ]]; then
     ginkgo -v -focus="import" -trace -debug import_cluster/import_cluster.test -- -v=3
 elif [[ $TEST_GROUP == "provision-all" ]]; then
     ginkgo -v -focus="create" --nodes=3 -trace -debug create_cluster/create_cluster.test -- -v=3 -owner="ginkgo-$TRAVIS_BUILD_ID" -cloud-providers=aws,azure,gcp
 elif [[ $TEST_GROUP == "destroy" ]]; then
-    ginkgo -v -focus="detach|destroy" -p -trace -debug detach_destroy/detach_destroy.test -- -v=3 -owner="ginkgo-$TRAVIS_BUILD_ID" -cloud-providers=aws,azure,gcp
+    ginkgo -v -focus="detach|destroy" --nodes=4 -trace -debug detach_destroy/detach_destroy.test -- -v=3 -owner="ginkgo-$TRAVIS_BUILD_ID" -cloud-providers=aws,azure,gcp
 elif [[ $TEST_GROUP == "metrics" ]]; then
     ginkgo -v -focus="metrics" -trace -debug metrics/metrics.test -- -v=3
 elif [[ $TEST_GROUP == "create-baremetal" ]]; then
@@ -19,4 +19,4 @@ elif [[ $TEST_GROUP == "destroy-baremetal" ]]; then
     ginkgo -v -focus="destroy" -trace -debug destroy_bm/destroy_bm.test -- -v=3 -cloud-providers=baremetal
 fi
 
-echo "Tests end at "$(date)
+echo "Tests end $TEST_GROUP at "$(date)
