c7186b210dfa46be3944533fef9d56adcb7973ce
Zhiwei Yin <zyin@redhat.com>
2022-01-14 22:34:19 +0800

    org rename (#77)
    Signed-off-by: Zhiwei Yin <zyin@redhat.com>

--- pkg/tests/destroy_bm/destry_bm_test.go
+++ pkg/tests/destroy_bm/destry_bm_test.go
@@ -4,7 +4,7 @@ package destroy_bm
 
 import (
 	. "github.com/onsi/ginkgo"
-	"github.com/open-cluster-management/cluster-lifecycle-e2e/pkg/utils"
+	"github.com/stolostron/cluster-lifecycle-e2e/pkg/utils"
 )
 
 var _ = Describe("Cluster-lifecycle: ", func() {
