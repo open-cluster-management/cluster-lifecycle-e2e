f0880abdefa89f44231ef694684def0e29bcac76
xuezhaojun <zxue@redhat.com>
2022-06-13 23:27:06 +0800

    replace "get -u" with "install"
    Signed-off-by: xuezhaojun <zxue@redhat.com>

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -12,14 +12,14 @@ export GO111MODULE=off
 
 # Go tools
 
-if ! which patter > /dev/null; then      echo "Installing patter ..."; go get -u github.com/apg/patter; fi
-if ! which gocovmerge > /dev/null; then  echo "Installing gocovmerge..."; go get -u github.com/wadey/gocovmerge; fi
+if ! which patter > /dev/null; then      echo "Installing patter ..."; go install github.com/apg/patter; fi
+if ! which gocovmerge > /dev/null; then  echo "Installing gocovmerge..."; go install github.com/wadey/gocovmerge; fi
 if ! which golangci-lint > /dev/null; then
    curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.23.6
 fi
 if ! which go-bindata > /dev/null; then
 	echo "Installing go-bindata..."
-	cd $(mktemp -d) && GOSUMDB=off go get -u github.com/go-bindata/go-bindata/...
+	cd $(mktemp -d) && GOSUMDB=off go install github.com/go-bindata/go-bindata/...
 fi
 go-bindata --version
 
